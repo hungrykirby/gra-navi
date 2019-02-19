9d424ffeebdd6e2eeb421b33d3318b3bef969d9d
katsuki hanawa <katsuki.hanawa@nomal.jp>
2019-02-20 05:34:30 +0900

    link osp and rakuten api

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -3,23 +3,27 @@ require 'uri'
 require 'json'
 
 class MapsController < ApplicationController
-    # @testに緯度経度の多重配列を渡す
     def oscmap
         rakuten_app_id = ENV['RAKUTEN_APP_ID']
 
-        url = URI.parse("https://app.rakuten.co.jp/services/api/Travel/SimpleHotelSearch/20170426?format=json&latitude=128440.51&longitude=503172.21&searchRadius=1&applicationId=" + rakuten_app_id)
-        puts url
+        @center_lat = 35.6812362
+        @center_lon = 139.7671248
+
+        url = URI.parse("https://app.rakuten.co.jp/services/api/Travel/SimpleHotelSearch/20170426?format=json&datumType=1&latitude=" + @center_lat.to_s + "&longitude=" + @center_lon.to_s + "&searchRadius=1&applicationId=" + rakuten_app_id)
+        #puts url
         json = Net::HTTP.get(url)
         result = JSON.parse(json)
-        for hotel in result['hotels'] do
-            #p hotel['hotel'][0]['hotelBasicInfo']
-            p hotel['hotel'][0]['hotelBasicInfo']['latitude']
-            p hotel['hotel'][0]['hotelBasicInfo']['longitude']
+        #p result
+        @yado_data = []
+        if result.has_key?('hotels') then
+            for hotel in result['hotels'] do
+                #p hotel['hotel'][0]['hotelBasicInfo']
+                lat = hotel['hotel'][0]['hotelBasicInfo']['latitude']
+                lon = hotel['hotel'][0]['hotelBasicInfo']['longitude']
+
+                @yado_data.push([lat, lon])
+            end
         end
-
-        @test = [[34.69, 135.49], [35.68, 139.69], [43, 141.35], [33.59, 130.40]]
-
-        #puts result
 
     end
 end
