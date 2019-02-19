2c611e127dbb7db9f10023b6e771c82fcb69a52b
katsuki hanawa <katsuki.hanawa@nomal.jp>
2019-02-20 00:59:47 +0900

    add rakuten travel api

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,9 +1,25 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
 class MapsController < ApplicationController
     # @testに緯度経度の多重配列を渡す
     def oscmap
-      @test = [[34.69, 135.49], [35.68, 139.69], [43, 141.35], [33.59, 130.40]]
+        rakuten_app_id = ENV['RAKUTEN_APP_ID']
 
-      @ln = ENV['LOGIN_NAME']
-      @lp = ENV['LOGIN_PASSWORD']
+        url = URI.parse("https://app.rakuten.co.jp/services/api/Travel/SimpleHotelSearch/20170426?format=json&latitude=128440.51&longitude=503172.21&searchRadius=1&applicationId=" + rakuten_app_id)
+        puts url
+        json = Net::HTTP.get(url)
+        result = JSON.parse(json)
+        for hotel in result['hotels'] do
+            #p hotel['hotel'][0]['hotelBasicInfo']
+            p hotel['hotel'][0]['hotelBasicInfo']['latitude']
+            p hotel['hotel'][0]['hotelBasicInfo']['longitude']
+        end
+
+        @test = [[34.69, 135.49], [35.68, 139.69], [43, 141.35], [33.59, 130.40]]
+
+        #puts result
+
     end
 end
